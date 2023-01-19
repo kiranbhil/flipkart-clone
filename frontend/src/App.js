cee75bd766d8a89478e496deccb6aeeea018ecbe
Yasin051197 <yasinjamadar0511@gmail.com>
2023-01-19 22:03:02 +0530

    day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Productdetailspage from './components/Productdetailspage';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Details from './components/Details';
 
 function App() {
   return (
     <div>
-      <Productdetailspage />
+      {/* <Productdetailspage /> */}
+            <Details /> 
     </div>
   );
 }
