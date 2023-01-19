b1f4d6b1d72bd781290741dbc7d1af52c48d4e2a
srinetanuj <srinetkumaranuj@gmail.com>
2023-01-20 00:25:00 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Productdetailspage from "./components/Productdetailspage";
+import NavButtom from "./components/Navbar/NavBottom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar/>
+      <NavButtom/>
       <Productdetailspage />
     </div>
   );
