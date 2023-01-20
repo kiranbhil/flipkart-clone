e0dd83390fc2de31d8a23e817080bf87a261ee06
Yasin051197 <yasinjamadar0511@gmail.com>
2023-01-21 00:31:49 +0530

    added some css

--- frontend/src/components/Productdetails/Details.jsx
+++ frontend/src/components/Productdetails/Details.jsx
@@ -1,23 +1,39 @@
+import { Box ,Heading,Typography} from '@chakra-ui/react'
 import React from 'react'
 import { useEffect } from 'react'
 import {useDispatch,useSelector} from "react-redux"
 import { useParams } from 'react-router-dom'
 import { getProductDetails } from '../../redux/actions/ProductAction'
+import ActionIteam from './ActionIteam'
 
 
 const Details = () => {
-  
+
 const dispatch=useDispatch()
 const {id}=useParams()
-// const {loading,product}= useSelector(state=> state.getProductDetails)
+const {loading,product}= useSelector(state=> state.getProductDetails)
 
   useEffect(()=>
   {
-    dispatch(getProductDetails(id))
-  },[dispatch,id])
+    if(product && id==product.id){
+      dispatch(getProductDetails(id))
+    }
+  },[dispatch,id,loading,product])
 
   return (
-    <div>Details</div>
+    <Box>
+      {loading && product && Object.keys(product).length&&
+      <Box>
+        <Box>
+          <ActionIteam product={product} />
+        </Box>
+        <Box>
+          <Heading>Title</Heading>
+        </Box>
+
+      </Box>
+     }
+    </Box>
   )
 }
 
