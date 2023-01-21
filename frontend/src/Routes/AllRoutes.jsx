c6ff21027d4a3d993ac0d7328eaa8e451d788a4f
Yasin051197 <yasinjamadar0511@gmail.com>
2023-01-21 22:05:11 +0530

    git add

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -15,12 +15,18 @@ const AllRoutes = () => {
           <Route path='/' element={<Homepage />}></Route>
           <Route path='/fashion' element={<Fashion />}></Route>
 <<<<<<< HEAD
+<<<<<<< HEAD
           <Route path='/product' element={<ChakraProvider> <Details /></ChakraProvider>}></Route>
 =======
+=======
+>>>>>>> 79263a4c97c9ff75154dd331b8897ea808083e7b
           <Route path='/cart' element={<EmptyCart />}></Route>
           <Route path='/products' element={<Products/>}></Route>
           <Route path='/products/product/:id' element={<ChakraProvider> <Productdetailspage /></ChakraProvider>}></Route>
           <Route path='/product/:id' element={<ChakraProvider> <Productdetailspage /></ChakraProvider>}></Route>
+<<<<<<< HEAD
+>>>>>>> 79263a4c97c9ff75154dd331b8897ea808083e7b
+=======
 >>>>>>> 79263a4c97c9ff75154dd331b8897ea808083e7b
         </Routes>
       </div>
--- frontend/src/components/Productdetails/ActionIteam.jsx
+++ frontend/src/components/Productdetails/ActionIteam.jsx
@@ -3,13 +3,14 @@ import { styled } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const LeftContainer=styled(Box)`
-min-width:40%
-padding:40px 0px 0px 80px
-`
+// const LeftContainer=styled(Box)`
+// min-width:40%
+// padding:40px 0px 0px 80px
+// `
 const ActionIteam = ({product}) => {
   return (
-    <LeftContainer>
+    // <LeftContainer>
+    <Box>
         <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80" />
         <Button variant="contained">ADD TO CART</Button>
         </Link>
