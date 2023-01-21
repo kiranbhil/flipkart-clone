6085a85874bf0e75e574d3826c9d3c9ac8ebdc82
Yasin051197 <yasinjamadar0511@gmail.com>
2023-01-21 21:59:44 +0530

    added files

--- frontend/src/components/Productdetails/ActionIteam.jsx
+++ frontend/src/components/Productdetails/ActionIteam.jsx
@@ -1,6 +1,7 @@
 import { Box, Button } from '@chakra-ui/react'
 import { styled } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LeftContainer=styled(Box)`
 min-width:40%
@@ -14,7 +15,9 @@ const ActionIteam = ({product}) => {
   return (
     <LeftContainer>
         <Image src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80" />
+        <Link key={product.id} to={`cart/${product.id}`} style={{ textDecoration: "none" }}>
         <Button variant="contained">ADD TO CART</Button>
+        </Link>
         <Button variant="contained">BUY NOW</Button>
     </LeftContainer>
   )
--- frontend/src/components/Productdetails/Details.jsx
+++ frontend/src/components/Productdetails/Details.jsx
@@ -1,14 +1,18 @@
-import { Box,Grid,Heading} from '@chakra-ui/react'
-// import { styled } from '@mui/material'
+import { Box,Heading} from '@chakra-ui/react'
+import { Grid, styled } from '@mui/material'
 import React from 'react'
 import { useEffect } from 'react'
 import {useDispatch,useSelector} from "react-redux"
 import { useParams } from 'react-router-dom'
 import { getProductDetails } from '../../redux/actions/ProductAction'
 import ActionIteam from './ActionIteam'
-import "./Details.css"
 
-
+const Component=styled(Box)`
+backgrond:#F2F2F2`
+const Container=styled(Grid)`
+background:#FFFFFF;
+display:flex`;
+// const Rightcontainer=styled()
 const Details = () => {
 
 const dispatch=useDispatch()
@@ -25,28 +29,29 @@ const {loading,product}= useSelector(state=> state.getProductDetails)
   },[dispatch,id,loading,product])
 
   return (
-    // <Box>
-    //   {loading && product && Object.keys(product).length&&
-    //   <Box>
-    //     <Box>
-    //       <ActionIteam />
-    //     </Box>
-    //     <Box>
-    //       <Heading>Title</Heading>
-    //     </Box>
-    //   </Box>
-    //  }
-    // </Box>
-    <Box className='Details_of_single_product'>
-      <Grid className='Details_of_single_product_Grid_container'>
-        <Grid item lg={4} md={4} sm={8} xs={12}>
-           <ActionIteam />
-        </Grid>
-        <Grid item lg={8} md={8} sm={8} xs={12}>
+    <Box>
+      {loading && product && Object.keys(product).length&&
+      <Box>
+        <Box>
+          <ActionIteam />
+        </Box>
+        <Box>
           <Heading>Title</Heading>
-        </Grid>
-      </Grid>
+        </Box>
+
       </Box>
+     }
+    </Box>
+    // <Component>
+    //   <Container>
+    //     <Grid item lg={4} md={4} sm={8} xs={12}>
+    //        <ActionIteam />
+    //     </Grid>
+    //     <Grid item lg={8} md={8} sm={8} xs={12}>
+    //       <Heading>Title</Heading>
+    //     </Grid>
+    //   </Container>
+    // </Component>
   )
 }
 
