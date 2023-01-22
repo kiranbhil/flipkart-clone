78341bfb75ffa2ae6c3f2434016dd144095d9925
Nasir imam <soulpanther4@gmail.com>
2023-01-22 15:05:30 +0530

    added last day work

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,24 +1,33 @@
-import Homepage from '../components/Home/Homepage';
-import Fashion from '../components/Fashion/Fashion';
-import { Route, Routes } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react';
-import Products from '../components/Product/Product';
-import Productdetailspage from '../components/Productdetails/Productdetailspage';
-import EmptyCart from '../components/cart/cartemptypage';
-import Details from '../components/Productdetails/Details';
-
+import Homepage from "../components/Home/Homepage";
+import Fashion from "../components/Fashion/Fashion";
+import { Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Products from "../components/Product/Product";
+import Productdetailspage from "../components/Productdetails/Productdetailspage";
+import EmptyCart from "../components/cart/cartemptypage";
+import Details from "../components/Productdetails/Details";
+import Copyright from "../components/checkout/Copyright";
+import MainCart from "../components/cart/mainCart";
 
 const AllRoutes = () => {
-  
-    return (
-      <div>
-        <Routes>
-          <Route path='/' element={<Homepage />}></Route>
-          <Route path='/fashion' element={<Fashion />}></Route>
-          <Route path='/product/:id' element={<ChakraProvider> <Details /></ChakraProvider>}></Route>
-        </Routes>
-      </div>
-    )
-  }
-  
-  export default AllRoutes
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/fashion" element={<Fashion />}></Route>
+        <Route
+          path="/product/:id"
+          element={
+            <ChakraProvider>
+              <Details />
+            </ChakraProvider>
+          }
+        ></Route>
+        <Route path="/checkout" element={<Copyright />} />
+        <Route path="/cart" element={<MainCart />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
